302007eb605cfd3cfacf69e32da7ca2a47374dd6
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 13:27:55 -0300

    Agregar estilos y cambios a componente header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,39 +3,45 @@ import styles from '@/styles/Header.module.css';
 import Navbar from './Navbar';
 import SectionHome from './SectionHome';
 
-
 const Bienvenida = () => {
-  const [mostrarTitulo, setMostrarTitulo] = useState(false);
-  const [mostrarParrafo, setMostrarParrafo] = useState(false);
+  const [mostrarTexto, setMostrarTexto] = useState([]);
+  const textos = [
+    "¡Bienvenido a mi portafolio!",
+    "Aquí podrás explorar mis habilidades y proyectos en desarrollo web.",
+    "Soy desarrollador web y enfermero profesional apasionado por crear soluciones digitales y contribuir al bienestar de los demás.",
+    "Te invito a conocer más sobre mi trabajo y mis conocimientos."
+  ];
 
   useEffect(() => {
-    setMostrarTitulo(true);
-    const timer = setTimeout(() => {
-      setMostrarParrafo(true);
-    }, 2000);
-    return () => clearTimeout(timer);
+    let index = 0;
+    const mostrar = () => {
+      if (index < textos.length) {
+        setMostrarTexto((prev) => [...prev, textos[index]]);
+        index++;
+        setTimeout(mostrar, 4000); // Mostrar el siguiente texto después de 4 segundos
+      }
+    };
+
+    mostrar(); // Inicia la secuencia de mostrar texto
+
+    return () => {
+      setMostrarTexto([]); // Limpiar el estado al desmontar el componente
+    };
   }, []);
 
   return (
     <>
-    <Navbar/>
-    <header className={styles.header}>
-      <SectionHome/>
-      <div className={styles.content}>
-        <h1 className={`${styles.titulo} ${mostrarTitulo ? styles.fadeIn : ''}`}>
-          ¡Bienvenido a mi portafolio!
-          </h1>
-        <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
-          Aquí podrás explorar mis habilidades y proyectos en desarrollo web.      
-        </h2>
-        <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
-          Soy desarrollador web y enfermero profesional apasionado por crear soluciones digitales y contribuir al bienestar de los demás. 
-        </h2>
-        <p>
-          Te invito a conocer más sobre mi trabajo y mis conocimientos.
-          </p>
-      </div>
-    </header>
+      <Navbar />
+      <header className={styles.header}>
+        <SectionHome />
+        <div className={styles.content}>
+          {mostrarTexto.map((texto, index) => (
+            <h2 key={index} className={`${styles.subtitulo} ${styles.fadeIn}`}>
+              {texto}
+            </h2>
+          ))}
+        </div>
+      </header>
     </>
   );
 };
