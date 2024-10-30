23d9b05b9118d4da7a8e9f2a75aea3d6c10d700c
Victor Peinado <vpeinado263@gmail.com>
2024-10-30 13:06:46 -0300

    Insertar componente Main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from '@/styles/Header.module.css';
 import Navbar from './Navbar';
+import Main from './Main';
 
 
 const Bienvenida = () => {
@@ -19,21 +20,22 @@ const Bienvenida = () => {
     <>
     <Navbar/>
     <header className={styles.header}>
-    <div className={styles.content}>
-      <h1 className={`${styles.titulo} ${mostrarTitulo ? styles.fadeIn : ''}`}>
-        ¡Mi Mombre es Victor!
-      </h1>
-      <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
-        Aquí podrás explorar mis habilidades y proyectos en desarrollo web.
-     </h2>
-     {/*
-      <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
-        Soy desarrollador web y enfermero profesional apasionado por crear soluciones digitales y contribuir al bienestar de los demás. 
-      </h2>
-      <p>
-        Te invito a conocer más sobre mi trabajo y mis conocimientos.
-      </p> */}
-    </div>
+      <Main/>
+      <div className={styles.content}>
+        <h1 className={`${styles.titulo} ${mostrarTitulo ? styles.fadeIn : ''}`}>
+          ¡Mi Nombre es Victor!
+        </h1>
+        <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
+          Aquí podrás explorar mis habilidades y proyectos en desarrollo web.      
+        </h2>
+            {/*
+           <h2 className={`${styles.subtitulo} ${mostrarParrafo ? styles.fadeIn : ''}`}>
+          Soy desarrollador web y enfermero profesional apasionado por crear soluciones digitales y contribuir al bienestar de los demás. 
+           </h2>
+          <p>
+           Te invito a conocer más sobre mi trabajo y mis conocimientos.
+           </p> */}
+      </div>
     </header>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,10 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import localFont from "next/font/local";
-// import styles from "@/styles/Home.module.css";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Main from "@/components/Main";
 
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+
 
 export default function Home() {
   return (
@@ -28,46 +16,8 @@ export default function Home() {
         <link rel="icon" href="/maletin.svg" />
       </Head>
       <Header/>
-      <Main/>
-      {/* <div className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}>
-        <main className={styles.main}>
-          <Image
-            className={styles.foto}
-            src="/myPhoto.png"
-            alt="Foto personal"
-            width={290}
-            height={180}
-            priority
-          />
-      
-        <h2>Bienvenido a mi portafolio.</h2> 
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/file.svg"
-                alt="proyectos"
-                width={25}
-                height={25}
-              />
-              Proyectos Personales
-            </a>
-            <a
-              href="www.linkedin.com/in/victor-peinado1739."
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-             Contactame
-            </a>
-          </div>
-        </main>
-      </div> */}
+   
+
       <Footer/>
     </>
   );
