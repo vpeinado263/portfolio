e95d728e15aba4b4e2f89a07a2a23aec9404a764
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 19:51:39 -0300

    Finalizar componente LinkText

--- src/components/atoms/LinkText.js
+++ src/components/atoms/LinkText.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import Link from "next/link";
 
-const LinkText = () => {
+const LinkText = ({ href, children, className}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link href={href} className={className}>
+        {children}
+    </Link>
+  );
+};
 
 export default LinkText
