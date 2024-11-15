ac80f8b356ec4c0d656a17b4d814fe7229542c74
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 13:34:26 -0300

    Referenciar componentes en archivo Home.js

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import Footer from "../organisms/Footer"
+import Header from "../organisms/Header"
+import SectionHome from "../organisms/SectionHome"
+
 
 const Home = () => {
   return (
     <div>
-      
+        <Header/>
+        <SectionHome/>
+        <Footer/>
     </div>
   )
 }
