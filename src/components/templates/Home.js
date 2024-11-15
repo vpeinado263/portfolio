c529091d1cf8211f5f86b36c06265536745d33fb
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 17:35:06 -0300

    Insertar className e inportar hoja de estilos

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,13 +1,15 @@
 import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
 import Navbar from "../organisms/Navbar";
+import syles from "Home.module.css";
 
 
 const Home = () => {
   return (
-    <div>
+    <div className={style.container}>
         <Header/>
         <Navbar/>
+        <div className={styles.mainContainer}></div>
         <Footer/>
     </div>
   )
