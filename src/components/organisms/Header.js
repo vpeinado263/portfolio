d0d2a167b07d9ecf115b9f4b935a53a50f8c3ac4
Victor Peinado <vpeinado263@gmail.com>
2024-11-14 21:57:23 -0300

    Modificar tiempo de carga de contenido

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -6,20 +6,10 @@ import SectionHome from './SectionHome';
 
 const Bienvenida = () => {
   const [mostrarTexto, setMostrarTexto] = useState([]);
-  
-  // Uso de useMemo para memorizar el array de secciones
   const secciones = useMemo(() => [
     {
-      titulo: "¡Bienvenido a mi portafolio!",
-      contenido: "Soy un Desarrollador Web en formación con experiencia en enfermería. Me apasiona encontrar soluciones digitales efectivas que puedan aplicarse en diversos sectores, desde la salud hasta cualquier desafío empresarial."
-    },
-    {
-        titulo: "Sobre mí",
-        contenido: "Soy un Desarrollador Web Full Stack en formación y Enfermero Profesional. A lo largo de mi carrera, he trabajado en proyectos Full Stack, y mi objetivo es aplicar mis habilidades tanto en el desarrollo web como en otros campos, ayudando a las empresas a resolver problemas mediante soluciones digitales innovadoras."
-    },
-    {
-      titulo: "Mi Misión",
-      contenido:  "Mi misión es aplicar mis conocimientos en desarrollo web y mi experiencia en el ámbito de la salud para crear soluciones digitales que impacten positivamente en cualquier área. Estoy comprometido con el aprendizaje continuo y con aportar valor a las empresas, independientemente de su sector, utilizando tecnología avanzada para resolver problemas."
+      titulo: "Bienvenido a mi portafolio",
+      contenido: "Soy un Desarrollador Web Full Stack en formación y Enfermero Profesional. Mi objetivo es aplicar mis habilidades tanto en el desarrollo web como en diversos sectores, buscando siempre crear soluciones digitales innovadoras que resuelvan problemas de cualquier área, desde la salud hasta desafíos empresariales. Estoy comprometido con el aprendizaje continuo y con aportar valor a las empresas utilizando tecnología avanzada."
     }
   ], []);
 
@@ -37,12 +27,12 @@ const Bienvenida = () => {
             gsap.fromTo(
               textRefs.current[currentIndex],
               { opacity: 0, y: 50 },
-              { opacity: 1, y: 0, duration: 2, ease: 'power2.out' }
+              { opacity: 1, y: 0, duration: 1, ease: 'power2.out' }
             );
           }
           index++;
           mostrar();
-        }, 3000);
+        }, 5000); 
       }
     };
 
@@ -55,15 +45,15 @@ const Bienvenida = () => {
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <header className={styles.header}>
-        <SectionHome/>
+        <SectionHome />
         <div className={styles.content}>
           {mostrarTexto.length > 0 && mostrarTexto.map((texto, index) => (
             <div 
               key={index} 
               ref={(el) => (textRefs.current[index] = el)} 
-              className={styles.subtitulo} // Se eliminó el uso de template strings ya que no es necesario aquí
+              className={styles.subtitulo}
             >
               <h2>{texto.titulo}</h2>
               <p>{texto.contenido}</p>
