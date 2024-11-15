be4b20f0a65a5c9fcb4d56a433ebb22e2b81c008
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 17:38:46 -0300

    Retraer cambios adicionales

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,15 +1,12 @@
 import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
 import Navbar from "../organisms/Navbar";
-import syles from "Home.module.css";
-
 
 const Home = () => {
   return (
-    <div className={style.container}>
+    <div>
         <Header/>
         <Navbar/>
-        <div className={styles.mainContainer}></div>
         <Footer/>
     </div>
   )
