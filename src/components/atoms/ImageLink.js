b60e6e4bdff50780f64e3d98f578f5f114a93c15
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 18:36:53 -0300

    Finalizar componente atomico para whatsapp

--- src/components/atoms/ImageLink.js
+++ src/components/atoms/ImageLink.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
-const ImageLink = () => {
+const ImageLink = ({ href, src, alt, width, height}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link href={href}>
+        <Image src={src} alt={alt} width={width} height={height}/>
+    </Link>
+  );
+};
 
-export default ImageLink
+export default ImageLink;
