773d362ff5d992cc5f4eac699b9bdb09dff403ce
Victor Peinado <vpeinado263@gmail.com>
2024-11-15 21:04:01 -0300

    Crear componente molecular Menu.js

---
+++ src/components/molecules/Menu.js
@@ -0,0 +1,13 @@
+import LinkItem from "../atoms/LinkItem";
+
+const Menu = () => {
+  return (
+   <ul>
+    <LinkItem href="#about" text="Lenguajes"/>
+    <LinkItem href="#skills" text="Tegnologías"/>
+    <LinkItem href="#projectc" text="Herramientas"/>
+   </ul>
+  );
+};
+
+export default Menu;
