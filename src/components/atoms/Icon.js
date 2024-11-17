c2c2b369c28be6701ad2986aaa0face5a4942b9d
Victor Peinado <vpeinado263@gmail.com>
2024-11-16 22:45:59 -0300

    Agregar estilos a componente Icon.js

--- src/components/atoms/Icon.js
+++ src/components/atoms/Icon.js
@@ -1,8 +1,12 @@
+import Image from "next/image";
+import styles from "@/styles/Icon.module.css";
 
-import Image from "next/image"
-
-const Icon = ({ src, alt, width, height}) => {
-  return <Image src={src} alt={alt} width={width} height={height}/>;
-}
+const Icon = ({ src, alt, width, height, className }) => {
+  return (
+    <div className={`${styles.icon} ${className || ""}`}>
+      <Image src={src} alt={alt} width={width} height={height} />
+    </div>
+  );
+};
 
 export default Icon;
