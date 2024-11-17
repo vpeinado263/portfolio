195ec0ac1918f0bb691f74b7de69c027d7a455ea
Victor Peinado <vpeinado263@gmail.com>
2024-11-16 22:23:54 -0300

    Incorporar clases para estilos

--- src/components/atoms/WhatsappLink.js
+++ src/components/atoms/WhatsappLink.js
@@ -1,10 +1,17 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/WhatsappLink.module.css";
 
-const WhatsappLink = ({ href, src, alt, width, height}) => {
+const WhatsappLink = ({ href, src, alt, width, height }) => {
   return (
-    <Link href={href}>
-        <Image src={src} alt={alt} width={width} height={height}/>
+    <Link href={href} className={styles.link}>
+      <Image
+        className={styles.image}
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+      />
     </Link>
   );
 };
