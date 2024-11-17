42b525319400dee3dbb02f75b44f20405e647ddb
Victor Peinado <vpeinado263@gmail.com>
2024-11-17 00:24:44 -0300

    Insertar className para crear estilos

--- src/components/atoms/TypedText.js
+++ src/components/atoms/TypedText.js
@@ -1,12 +1,12 @@
 import { forwardRef } from "react";
+import styles from "@/styles/TypedText.module.css"; 
 
 const TypedText = forwardRef(({ text }, ref) => (
-  <h1 ref={ref} id="typedText">
+  <h1 ref={ref} id="typedText" className={styles.typedText}>
     {text}
   </h1>
 ));
 
-// Asigna un displayName
 TypedText.displayName = "TypedText";
 
 export default TypedText;
