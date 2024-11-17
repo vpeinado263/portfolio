9993d2fb1b8f42078546faf812be80d818afbbd5
Victor Peinado <vpeinado263@gmail.com>
2024-11-17 13:15:59 -0300

    Actualizar configuracion de next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
 export default nextConfig;
+
