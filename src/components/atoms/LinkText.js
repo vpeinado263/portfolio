d1212a78dbc4052b946e044127d9067d32cca545
Victor Peinado <vpeinado263@gmail.com>
2024-11-16 23:22:43 -0300

    Crear clases para componente e importar hoja de estilos

--- src/components/atoms/LinkText.js
+++ src/components/atoms/LinkText.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import styles from "@/styles/LinkText.module.css"; 
 
-const LinkText = ({ href, children, className}) => {
+const LinkText = ({ href, children, className }) => {
   return (
-    <Link href={href} className={className}>
-        {children}
+    <Link href={href} className={`${styles.link} ${className ? className : ''}`}>
+      {children}
     </Link>
   );
 };
 
-export default LinkText
+export default LinkText;
+
