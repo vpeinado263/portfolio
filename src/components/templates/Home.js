0957e60564979f1761947c059c7af603c047b9fd
Victor Peinado <vpeinado263@gmail.com>
2024-11-18 17:09:08 -0300

    Quitar componente Navbar y Fotter

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,6 +1,4 @@
-import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
-import Navbar from "../organisms/Navbar";
 
 const Home = () => {
 
@@ -9,8 +7,6 @@ const Home = () => {
   return (
     <div>
         <Header/> 
-        <Navbar/>
-        <Footer/>
     </div>
   )
 }
