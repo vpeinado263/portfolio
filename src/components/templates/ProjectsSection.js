0be477ea5d66c97eac26f33346ed3d41d6c3a850
Victor Peinado <vpeinado263@gmail.com>
2024-11-17 21:29:51 -0300

    Importar componente ProjectCategory.js crear estructura template

---
+++ src/components/templates/ProjectsSection.js
@@ -0,0 +1,21 @@
+import ProjectCategory from "../organisms/ProjectCategory";
+
+const ProjectsSection = ({data}) => {
+  return (
+    <section className="projects-section">
+    <h2 className="section-title">Proyectos</h2>
+    <div className="projects-container">
+      {data.map((categoryData, index) => (
+        <ProjectCategory
+          key={index}
+          category={categoryData.category}
+          description={categoryData.description}
+          projects={categoryData.projects}
+        />
+      ))}
+    </div>
+  </section>
+  )
+}
+
+export default ProjectsSection;
