e9fbcf96e6d0bf1408634f5232f014526e8267a0
Victor Peinado <vpeinado263@gmail.com>
2024-11-19 13:41:55 -0300

    Modificar componente Menu.js

--- src/components/molecules/Menu.js
+++ src/components/molecules/Menu.js
@@ -4,9 +4,9 @@ import styles from "@/styles/Menu.module.css";
 const Menu = () => {
   return (
    <ul className={styles.menu}>
-    <LinkItem href="/projectsPage" text="My Projects"/>
-    <LinkItem href="/skillPage" text="The Skillset"/>
-    <LinkItem href="/" text="Insights"/>
+    <LinkItem href="" text="My Projects"/>
+    <LinkItem href="" text="The Skillset"/>
+    <LinkItem href="" text="Insights"/>
    </ul>
   );
 };
