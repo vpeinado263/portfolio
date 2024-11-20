c77ea674547809a91ae7c7598e266325c98533ef
Victor Peinado <vpeinado263@gmail.com>
2024-11-20 17:41:13 -0300

    Finalzirar configuracionde boton hamburgurgesa

--- src/components/molecules/MyMenu.js
+++ src/components/molecules/MyMenu.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import LinkItem from "../atoms/LinkItem";
 import styles from "@/styles/MyMenu.module.css";
+import { FarBars } from 'react-icons';
 
 const MyMenu = () => {
  const [isMyMenuOpen, setIsMyMenuOpen] = useState(false);
- const toggleMenu = () => setIsMyMenuOpen(!isMyMenuOpen);
+ const toggleMyMenu = () => setIsMyMenuOpen(!isMyMenuOpen);
 
   return (
-   <ul className={styles.menu}>
-    <LinkItem href="" text="My Projects"/>
-    <LinkItem href="" text="The Skillset"/>
-    <LinkItem href="" text="Insights"/>
-   </ul>
+   <div className={styles.burgerContainer} >
+      <button className={styles.burger} onClick={toggleMyMenu}>
+       <FarBars size={30} color="white"/>  
+      </button>    
+ 
+      <ul className={`${styles.menu} ${isMyMenuOpen ? styles.activar : ""}`}>
+       <LinkItem href="" text="My Projects"/>
+       <LinkItem href="" text="The Skillset"/>
+       <LinkItem href="" text="Insights"/>
+      </ul>
+   </div>
   );
 };
 
