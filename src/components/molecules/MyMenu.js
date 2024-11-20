a42cbf69c9e5ff46c3e0d3c5b387df542fe79ed9
Victor Peinado <vpeinado263@gmail.com>
2024-11-20 17:29:43 -0300

    Implementar boton amburgesa

--- src/components/molecules/MyMenu.js
+++ src/components/molecules/MyMenu.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import LinkItem from "../atoms/LinkItem";
 import styles from "@/styles/MyMenu.module.css";
 
 const MyMenu = () => {
+ const [isMyMenuOpen, setIsMyMenuOpen] = useState(false);
+ const toggleMenu = () => setIsMyMenuOpen(!isMyMenuOpen);
+
   return (
    <ul className={styles.menu}>
     <LinkItem href="" text="My Projects"/>
