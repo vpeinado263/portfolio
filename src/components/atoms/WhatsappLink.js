cb49ebb0077a25f7c8eb9d23d229f570d43e9a5f
Victor Peinado <vpeinado263@gmail.com>
2024-11-24 17:35:33 -0300

    Insertar framer-motion en image whatsapp

--- src/components/atoms/WhatsappLink.js
+++ src/components/atoms/WhatsappLink.js
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/WhatsappLink.module.css";
+import { motion } from "framer-motion";
 
 const WhatsappLink = ({ href, src, alt, width, height }) => {
   return (
     <Link href={href} className={styles.link}>
-      <Image
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 5 }}
+      >
+        <Image
         className={styles.image}
         src={src}
         alt={alt}
         width={width}
         height={height}
       />
+      </motion.div>
     </Link>
   );
 };
