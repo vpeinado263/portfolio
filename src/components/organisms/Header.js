8f9c19c562f8eed3a518675ce922b43998c0cdeb
Victor Peinado <vpeinado263@gmail.com>
2024-11-24 17:25:31 -0300

    Aplicar framer-motion a componente header

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,12 +1,19 @@
 import styles from '@/styles/Header.module.css';
 import SectionHome from './SectionHome';
+import { motion } from 'framer-motion';
 
 const Header = () => {
 
   return (
-      <header className={styles.header}>
+    <motion.div
+    initial={{ opacity: 0 }}  
+    animate={{ opacity: 1 }}   
+    transition={{ duration: 2 }} 
+    >
+       <header className={styles.header}>
         <SectionHome />
       </header>
+    </motion.div>
   );
 };
 
