0e610d27bfa7adcd2736bc7d1dd637883a37d2ac
Victor Peinado <vpeinado263@gmail.com>
2024-11-24 20:21:48 -0300

    Modificar componete Logo.js y hoja de estilos

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 import styles from "@/styles/Logo.module.css";  
 
 const Logo = () => {
   return (
     <Link href="/" className={styles.logo}>
-      <h2 className={styles.logoTitle}>Victor Peinado </h2>
-      <h3 className={styles.logoTitle}>Portfolio</h3>
+      <motion.div 
+        className={styles.logoContainer}
+        initial={{ opacity: 0, scale: 0.8 }}  
+        animate={{ opacity: 1, scale: 1 }}   
+        transition={{ duration: 1, ease: "easeOut" }}  
+      >
+         <h2 className={styles.logoTitle}>Victor Peinado </h2>
+         <h3 className={styles.logoTitle}>Portfolio</h3>
+      </motion.div>
     </Link>
   );
 };
