0125ecd395a4ad46802bed3f993b27d15f219155
Victor Peinado <vpeinado263@gmail.com>
2024-11-25 07:52:53 -0300

    Eliminar componente CTAButtons.js

--- src/components/molecules/CTAButtons.js
+++ src/components/molecules/CTAButtons.js
@@ -1,19 +1,10 @@
-import Button from '../atoms/Button';
 import Whatsapp from './Whatsapp';
-import Image from 'next/image';
 import styles from '@/styles/CTAButtons.module.css';
 
 const CTAButtons = ({ whatsappMessage }) => {
   return (
     <div className={styles.containerButton}>
-      <Button
-        href="https://github.com/vpeinado263"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image src="/file.svg" alt="proyectos" width={25} height={25} />
-        Projects on GitHub
-      </Button>
+      
       <span className={styles.secondary}>
         Whatsapp
         <Whatsapp message={whatsappMessage} />
