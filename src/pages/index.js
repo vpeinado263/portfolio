cded2f7a87004d9faa7e111b8435926ebc9d1e65
Victor Peinado <vpeinado263@gmail.com>
2024-11-27 16:55:11 -0300

    Eliminar componentes de Habilidades

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,141 +3,8 @@ import Head from 'next/head';
 import Navbar from '@/components/organisms/Navbar';
 import Home from '@/components/templates/Home';
 import Footer from '@/components/organisms/Footer';
-
-import {
-  FaReact,
-  FaNodeJs,
-  FaGit,
-  FaHtml5,
-  FaCss3Alt,
-  FaGithub,
-  FaSlack,
-  FaBootstrap,
-} from 'react-icons/fa';
-import {
-  SiJavascript,
-  SiTypescript,
-  SiNextdotjs,
-  SiExpress,
-  SiMongodb,
-  SiMysql,
-  SiGraphql,
-  SiJquery,
-  SiAxios,
-  SiTailwindcss,
-  SiPostman,
-  SiNpm,
-  SiHeroku,
-  SiVercel,
-} from 'react-icons/si';
-import {
-  FaClipboardList,
-  FaTasks,
-  FaUsers,
-  FaComments,
-  FaLightbulb,
-} from 'react-icons/fa';
-import Projects from '@/components/templates/Projects';
 import BienvSpinnerTemp from '@/components/templates/BienvSpinnerTemp';
-import SkillChart from '@/components/atoms/SkillChart';
-
-const skillsIcons = {
-  JavaScript: <SiJavascript />,
-  HTML: <FaHtml5 />,
-  CSS: <FaCss3Alt />,
-  TypeScript: <SiTypescript />,
-  React: <FaReact />,
-  'Next.js': <SiNextdotjs />,
-  'Express.js': <SiExpress />,
-  jQuery: <SiJquery />,
-  Axios: <SiAxios />,
-  Bootstrap: <FaBootstrap />,
-  'Tailwind CSS': <SiTailwindcss />,
-  'Node.js': <FaNodeJs />,
-  MongoDB: <SiMongodb />,
-  MySQL: <SiMysql />,
-  'REST APIs': <FaClipboardList />,
-  GraphQL: <SiGraphql />,
-  Git: <FaGit />,
-  GitHub: <FaGithub />,
-  NPM: <SiNpm />,
-  Postman: <SiPostman />,
-  Slack: <FaSlack />,
-  Vercel: <SiVercel />,
-  Heroku: <SiHeroku />,
-  Scrum: <FaClipboardList />,
-  Kanban: <FaTasks />,
-  'Trabajo en equipo': <FaUsers />,
-  Comunicación: <FaComments />,
-  'Resolución de problemas': <FaLightbulb />,
-};
-
-const skillsData = [
-  {
-    category: 'Lenguajes de Programación',
-    skills: [
-      { name: 'JavaScript', value: 80 },
-      { name: 'HTML', value: 90 },
-      { name: 'CSS', value: 85 },
-      { name: 'TypeScript', value: 70 },
-    ],
-  },
-  {
-    category: 'Frameworks',
-    skills: [
-      { name: 'React', value: 90 },
-      { name: 'Next.js', value: 75 },
-      { name: 'Express.js', value: 80 },
-    ],
-  },
-  {
-    category: 'Librerías',
-    skills: [
-      { name: 'jQuery', value: 60 },
-      { name: 'Axios', value: 80 },
-      { name: 'Bootstrap', value: 85 },
-      { name: 'Tailwind CSS', value: 90 },
-    ],
-  },
-  {
-    category: 'Tecnologías',
-    skills: [
-      { name: 'Node.js', value: 80 },
-      { name: 'MongoDB', value: 70 },
-      { name: 'MySQL', value: 75 },
-      { name: 'REST APIs', value: 85 },
-      { name: 'GraphQL', value: 70 },
-    ],
-  },
-  {
-    category: 'Herramientas',
-    skills: [
-      { name: 'Git', value: 90 },
-      { name: 'GitHub', value: 85 },
-      { name: 'NPM', value: 80 },
-      { name: 'Postman', value: 70 },
-      { name: 'Slack', value: 75 },
-      { name: 'Vercel', value: 80 },
-      { name: 'Heroku', value: 70 },
-    ],
-  },
-  {
-    category: 'Metodologías',
-    skills: [
-      { name: 'Scrum', value: 80 },
-      { name: 'Kanban', value: 75 },
-    ],
-  },
-  {
-    category: 'Soft Skills',
-    skills: [
-      { name: 'Trabajo en equipo', value: 90 },
-      { name: 'Comunicación', value: 85 },
-      { name: 'Resolución de problemas', value: 80 },
-    ],
-  },
-];
-
+import Projects from '@/components/templates/Projects';
 
 export default function Index() {
   const [isLoading, setIsLoading] = useState(true);
@@ -159,13 +26,12 @@ export default function Index() {
         <link rel="icon" href="/maletin.svg" />
       </Head>
       {isLoading ? (
-        <BienvSpinnerTemp />
+        <BienvSpinnerTemp/>
       ) : (
         <>
           <Navbar />
           <Home />
-          <SkillChart/>
-          <Projects />
+          <Projects/>
           <Footer />
         </>
       )}
