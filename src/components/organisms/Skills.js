9fe495175847fce1550c2c2f2c966ff283d6bd95
Victor Peinado <vpeinado263@gmail.com>
2024-11-27 14:38:54 -0300

    Referenciar componente en index.js

--- src/components/organisms/Skills.js
+++ src/components/organisms/Skills.js
@@ -1,10 +1,18 @@
 import styles from '@/styles/Skills.module.css';
 import SkillCategory from '@/components/molecules/SkillCategory';
+import SkillChart from '@/components/atoms/SkillChart';
 
 const Skills = ({ skillsData, skillsIcons }) => (
   <section id="skillset">
     <div className={styles.skillsSection}>
       <h2 className={styles.skillsTitle}>Habilidades y Conocimientos</h2>
+
+      <div className={styles.skillsContainer}>
+        <div className={styles.chartContainer}>
+          <h3>Gráfico de habilidades</h3>
+          <SkillChart/>
+        </div>
+      </div>
 
       <div className={styles.skillsContainer}>
         {skillsData.map((skill) => (
