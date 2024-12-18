6495885736ca4e2532b8cf66ce5f4bdbef8d89ae
Victor Peinado <vpeinado263@gmail.com>
2024-12-18 00:46:31 -0300

    Actualizar curriculum vitae

--- src/components/molecules/MyMenu.js
+++ src/components/molecules/MyMenu.js
@@ -21,17 +21,17 @@ const MyMenu = () => {
         </button>
       </div>
 
-       <motion.div
+      <motion.div
         className={styles.logoContainer}
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1, ease: 'easeOut' }}
       >
-      <ul className={`${styles.menu} ${isMyMenuOpen ? styles.activar : ''}`}>
-        <LinkItem href="#My Projects" text="Projects" />
-        <LinkItem href="#About" text="About" />
-        <LinkItem href="" text="Insights" />
-      </ul>
+        <ul className={`${styles.menu} ${isMyMenuOpen ? styles.activar : ''}`}>
+          <LinkItem href="#My Projects" text="Projects" />
+          <LinkItem href="#About" text="About" />
+          <LinkItem href="" text="Insights" />
+        </ul>
       </motion.div>
     </>
   );
