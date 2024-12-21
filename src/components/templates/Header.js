60693ac8255f3e5b677933355418df26f1ca0fbe
Victor Peinado <vpeinado263@gmail.com>
2024-12-20 22:19:06 -0300

    Corregir codigo con prettier

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -4,13 +4,13 @@ import Navbar from '../organisms/Navbar';
 const Header = () => {
   return (
     <header>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 2 }}
-          transition={{ duration: 3 }}
-        >
-          <Navbar />
-        </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 2 }}
+        transition={{ duration: 3 }}
+      >
+        <Navbar />
+      </motion.div>
     </header>
   );
 };
