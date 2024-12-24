ba539036e2e53429334402f28eabba29affcac70
Victor Peinado <vpeinado263@gmail.com>
2024-12-24 12:42:36 -0300

    Finalizar Implmentacion de imagen en header.js

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -1,10 +1,24 @@
+import { ThemeContext } from '@/context/ThemeContextProvider';
+import { useContext } from 'react';
 import { motion } from 'framer-motion';
 import Footer from '../organisms/Footer';
 import Navbar from '../organisms/Navbar';
+import Image from 'next/image';
 
 const Header = () => {
+  const data = useContext(ThemeContext);
+  const [toggleTheme, handleTheme] = data;
+
   return (
-    <header>
+    <header className={toggleTheme}>
+      <button onClick={handleTheme}>
+        <Image
+        src={toggleTheme === "light" ? "/sun.png" : "/sunDark.png"}
+        alt="Theme"
+        width={30}
+        height={30}
+        />
+      </button>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 2 }}
