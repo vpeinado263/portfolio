bedac7006e02a9122f9d5bc4ffe978c9c4880ce5
Victor Peinado <vpeinado263@gmail.com>
2024-12-24 12:13:33 -0300

    Insertar cambios a componente projects.js

--- src/components/templates/Projects.js
+++ src/components/templates/Projects.js
@@ -9,12 +9,14 @@ const projectsData = [
       'Proyectos simples que me ayudaron a entender la estructura básica de las páginas web y la semántica de HTML.',
     projects: [
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-1-clase-html.vercel.app/',
         icon: (
           <FaHtml5 style={{ color: 'rgb(233, 98, 40)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-2-clase-html.vercel.app/',
         icon: (
           <FaHtml5 style={{ color: 'rgb(233, 98, 40)', fontSize: '2rem' }} />
@@ -28,30 +30,35 @@ const projectsData = [
       'Aprendí sobre estilos, diseño responsivo, y cómo dar vida a las interfaces.',
     projects: [
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-3-clase-css.vercel.app/',
         icon: (
           <FaCss3Alt style={{ color: 'rgb(21, 114, 182)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-4-clase-css.vercel.app/',
         icon: (
           <FaCss3Alt style={{ color: 'rgb(21, 114, 182)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-5-clase-css.vercel.app/',
         icon: (
           <FaCss3Alt style={{ color: 'rgb(21, 114, 182)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-6-clase-css.vercel.app/',
         icon: (
           <FaCss3Alt style={{ color: 'rgb(21, 114, 182)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://tarea-7-clase-css.vercel.app/',
         icon: (
           <FaCss3Alt style={{ color: 'rgb(21, 114, 182)', fontSize: '2rem' }} />
@@ -65,6 +72,7 @@ const projectsData = [
       'Desarrollé habilidades de programación, manejo de APIs y lógica en proyectos interactivos.',
     projects: [
       {
+        title: 'Peso Argentino',
         link: 'https://peso-argentino.vercel.app/',
         icon: (
           <FaJsSquare
@@ -73,6 +81,7 @@ const projectsData = [
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1', 
         link: 'https://consumiendo-api-rest-contries.vercel.app/',
         icon: (
           <FaJsSquare
@@ -81,6 +90,7 @@ const projectsData = [
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 1',
         link: 'https://consumiendo-api-rest-contries-yhyq.vercel.app/',
         icon: (
           <FaJsSquare
@@ -89,6 +99,7 @@ const projectsData = [
         ),
       },
       {
+        title: 'Consumiendo API Rest Countries 2',
         link: 'https://herramientas-de-precisi-n.vercel.app/',
         icon: (
           <FaJsSquare
@@ -97,6 +108,7 @@ const projectsData = [
         ),
       },
       {
+        title: 'Herramientas de Precisión',
         link: 'https://notas-interactivas.vercel.app/',
         icon: (
           <FaJsSquare
@@ -105,6 +117,7 @@ const projectsData = [
         ),
       },
       {
+        title: 'Notas Interactivas',
         link: 'https://nasa-api-wheat.vercel.app/',
         icon: (
           <FaJsSquare
@@ -120,42 +133,49 @@ const projectsData = [
       'Implementé componentes dinámicos, manejo de rutas y despliegues modernos.',
     projects: [
       {
+        title: 'Figuritas',
         link: 'https://2da-tarea-react-next-js.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Relojes',
         link: 'https://4ta-tarea-react-next-js.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Modo-claro-oscuro',
         link: 'https://5ta-tarea-react-next-js.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Shopping-Cart',
         link: 'https://6ta-tarea-react-next-1e3yvlp76-vhp-projects-694c293c.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Breaking-Bad',
         link: 'https://7ma-tarea-react-next-js.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Infinito JG',
         link: 'https://infinito-jg.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
         ),
       },
       {
+        title: 'Tienda - X -',
         link: 'https://tienda-x--swart.vercel.app/',
         icon: (
           <FaReact style={{ color: 'rgb(18, 155, 194)', fontSize: '2rem' }} />
@@ -172,7 +192,7 @@ const Projects = () => {
         <div key={index} className={styles.projectCategory}>
           <h3 className={styles.categoryTitle}>{category.category}</h3>
           <p className={styles.categoryDescription}>{category.description}</p>
-          <ul className={styles.projectList}>
+          <ul>
             {category.projects.map((project, idx) => (
               <li key={idx}>
                 <Link
@@ -180,7 +200,7 @@ const Projects = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <div className={styles.projectIcon}>{project.icon}</div>
+                  <div>{project.icon}</div>
                   <p>{project.title}</p>
                 </Link>
               </li>
