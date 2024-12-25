557ba0a3e704462b4bcaf705d6ad95521b7c50bf
Victor Peinado <vpeinado263@gmail.com>
2024-12-25 15:50:08 -0300

    Ajustar contecto theme claroscuro

--- src/context/ThemeContextProvider.js
+++ src/context/ThemeContextProvider.js
@@ -1,22 +1,39 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext()
 
 const ThemeContextProvider = ({children}) => {
-    const [theme, setTheme] = useState(false)
-
-    const handleTheme = () => {
-        setTheme(!theme)
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem("theme");
+      return storedTheme ? storedTheme === "dark" : false;
     }
-    
-    const toggleTheme = theme ? "dark" : "light"
-    const data = [toggleTheme, handleTheme]
+    return false;
+  });
+
+  useEffect(() => {
+    const rootElement = document.documentElement; 
+    if (theme) {
+      rootElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      rootElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme((prevTheme) => !prevTheme);
+  };
+
+    const toggleTheme = theme ? "dark" : "light";
+    const data = [toggleTheme, handleTheme];
 
   return (
     <ThemeContext.Provider value={data}>
       {children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
 export default ThemeContextProvider;
