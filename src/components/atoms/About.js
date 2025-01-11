14524990aa0824cedd3a95d40a7e6f16383728cc
Victor Peinado <vpeinado263@gmail.com>
2025-01-11 00:29:55 -0300

    Agregar modificaciones a About

--- src/components/atoms/About.js
+++ src/components/atoms/About.js
@@ -9,24 +9,26 @@ const About = () => {
           Soy un profesional con una combinación única de habilidades y
           pasiones. Como enfermero, trabajé durante la pandemia COVID-19 en el
           Hospital Central de Mendoza, brindando atención crítica a pacientes en
-          un entorno desafiante y exigente. Además, soy desarrollador web Full
-          Stack, con un enfoque en React en creación de aplicaciones dinámicas.
+          un entorno desafiante y exigente. Además, soy Desarrollador web en formación, 
+          con experiencia en proyectos personales que abarcan desde el diseño de interfaces 
+          hasta la implementación de back-end con Node.js y MongoDB, con un enfoque en React
+          en creación de aplicaciones dinámicas.
           Disfruto diseñar soluciones tecnológicas que combinen funcionalidad y
           estética.
         </p>
         <p>
           Fuera del ámbito profesional, encuentro inspiración en la forja. Mi
           pasatiempo es forjar de cuchillos, un arte que me permite explorar la
-          precisión, la creatividad y el detalle, valores que también aplico en
-          mi trabajo como enfermero y desarrollador. Este equilibrio entre
+          precisión, la creatividad y el detalle. Este equilibrio entre
           tecnología, salud y manualidades refleja mi compromiso con la
           innovación y la dedicación en cada proyecto que emprendo.
         </p>
         <p>
           Mi objetivo es continuar creciendo en ambos mundos, desarrollando
           herramientas tecnológicas que impacten positivamente en la calidad de
-          vida de las personas y perfeccionando mis habilidades. Estoy en
-          constante aprendizaje, explorando nuevas tecnologías, herramientas y
+          vida de las personas y perfeccionando mis habilidades. Busco mi primera experiencia laboral en
+          desarrollo full stack para aplicar y seguir ampliando mis conocimientos. 
+          Estoy en constante aprendizaje, explorando nuevas tecnologías, herramientas y
           técnicas para lograrlo.
         </p>
       </div>
