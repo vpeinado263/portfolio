d3c6231c12ead7b222872cab308ebd90ddb21837
Victor Peinado <vpeinado263@gmail.com>
2025-08-04 22:35:09 -0300

    Ejecutar prettier

--- src/components/atoms/About.js
+++ src/components/atoms/About.js
@@ -16,21 +16,22 @@ const About = () => {
           dinámicas.
         </p>
         <p>
-          A lo largo de mi trayectoria profesional he desarrollado una gran capacidad 
-          para comunicarme con personas que atraviesan situaciones estresantes, 
-          críticas o de alto impacto emocional.
-          Estoy acostumbrado a interactuar con usuarios que expresan su malestar de forma
-          intensa incluso con enojo y sé responder con calma, empatía y eficacia.
-          Considero que una comunicación efectiva, respetuosa y clara es fundamental para 
-          resolver situaciones urgentes y restaurar la confianza, especialmente cuando se 
-          trata de servicios sensibles o emergencias.
+          A lo largo de mi trayectoria profesional he desarrollado una gran
+          capacidad para comunicarme con personas que atraviesan situaciones
+          estresantes, críticas o de alto impacto emocional. Estoy acostumbrado
+          a interactuar con usuarios que expresan su malestar de forma intensa
+          incluso con enojo y sé responder con calma, empatía y eficacia.
+          Considero que una comunicación efectiva, respetuosa y clara es
+          fundamental para resolver situaciones urgentes y restaurar la
+          confianza, especialmente cuando se trata de servicios sensibles o
+          emergencias.
         </p>
         <p>
-          Estoy dando mis primeros pasos en el mundo IT, aplicando los 
-          conocimientos adquiridos en mi formación como Desarrollador Web Full Stack.
-          Me encuentro en constante aprendizaje, creando proyectos propios, explorando 
-          nuevas tecnologías y perfeccionando herramientas y técnicas que me permitan 
-          crecer y aportar valor en entornos reales...
+          Estoy dando mis primeros pasos en el mundo IT, aplicando los
+          conocimientos adquiridos en mi formación como Desarrollador Web Full
+          Stack. Me encuentro en constante aprendizaje, creando proyectos
+          propios, explorando nuevas tecnologías y perfeccionando herramientas y
+          técnicas que me permitan crecer y aportar valor en entornos reales...
         </p>
       </div>
     </section>
