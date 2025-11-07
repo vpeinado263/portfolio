3c8ca063d462d0f257b41771cca87a9a3efce0b2
Victor Peinado <vpeinado263@gmail.com>
2025-11-07 17:10:38 -0300

    Agregaar cambios eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,41 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
+import prettierPlugin from "eslint-plugin-prettier";
+import nextPlugin from "@next/eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "jsx-a11y": jsxA11yPlugin,
+      prettier: prettierPlugin,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...jsxA11yPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
 ]);
