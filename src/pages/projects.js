72bebe425b8314009c8cc240a3cd4cab30cbcfcc
Victor Peinado <vpeinado263@gmail.com>
2026-03-05 12:31:36 -0300

    Agragar modiificaciones en Navbar

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NextSeo } from "next-seo";
 import Footer from "@/components/organisms/Footer";
-import Insights from '@/components/templates/Insights';
+import Projects from '@/components/templates/Projects';
+
 
 const projects = () => {
   return (
@@ -9,7 +10,7 @@ const projects = () => {
     <NextSeo title="Insights | Victor Peinado" />
       <main className="min-h-screen bg-linear-to-b from-gray-50 to-white dark:from-gray-950 dark:to-gray-900 pt-20 lg:pt-0">
         <div className="lg:ml-(--navbar-width) xl:ml-(--navbar-width-lg)">
-          <Insights />
+          <Projects />
         </div>
       </main>
       <Footer />
